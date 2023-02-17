7ea108d042c3a5fceac440b42e9d58db9f3dd3d3
Konstantin Mishakov <mishakov1c@gmail.com>
2023-02-17 23:10:51 +0700

    Seventh task

--- 7_exception2.py
+++ 7_exception2.py
@@ -15,9 +15,9 @@
 
 def discounted(price, discount, max_discount=20):
     try:
-        price = float(abs(price))
-        discount = float(abs(discount))
-        max_discount = int(abs(max_discount))    
+        price = abs(float(price))
+        discount = abs(float(discount))
+        max_discount = abs(int(max_discount))    
         if max_discount >= 100:
             raise ValueError('Слишком большая максимальная скидка')
         if discount >= max_discount:
