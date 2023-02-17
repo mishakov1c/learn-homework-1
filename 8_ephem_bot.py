aca3300ea06bb92b4225ffae9fa7f267798193e4
Konstantin Mishakov <mishakov1c@gmail.com>
2023-02-18 01:50:01 +0700

    Eighth task

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -13,7 +13,9 @@
 
 """
 import logging
-
+import settings
+import datetime
+import ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -21,32 +23,63 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log')
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
-
-
 def greet_user(update, context):
     text = 'Вызван /start'
     print(text)
     update.message.reply_text(text)
 
-
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
 
+def get_ephem_planet(planet_name):
+    now = datetime.datetime.now()
+
+    if planet_name == 'Mars':
+        ephem_planet = ephem.Mars(now)
+    elif planet_name == 'Mercury':
+         ephem_planet = ephem.Mercury(now)
+    elif planet_name == 'Venus':
+        ephem_planet = ephem.Venus(now)
+    elif planet_name == 'Earth':
+        ephem_planet = ephem.Earth(now)
+    elif planet_name == 'Jupiter':
+        ephem_planet = ephem.Jupiter(now)
+    elif planet_name == 'Uranus':
+        ephem_planet = ephem.Uranus(now)
+    elif planet_name == 'Neptune':
+        ephem_planet = ephem.Neptune(now)
+
+    return ephem_planet
+
+def planet_info(update, context):
+    user_text = update.message.text
+    print(user_text) 
+    
+    splitted_text = user_text.split()
+    
+    try:
+        planet_name = splitted_text[1]
+        print(planet_name)
+        ephem_planet = get_ephem_planet(planet_name)
+        const = ephem.constellation(ephem_planet)
+        constellation = const[1]
+        update.message.reply_text(f'Планета "{planet_name}" находится в созвездии "{constellation}"')   
+    except IndexError:
+        error_text = "Имя планеты не введено" 
+        print(error_text)
+        update.message.reply_text(error_text)
+
+
+    
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet_info))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
